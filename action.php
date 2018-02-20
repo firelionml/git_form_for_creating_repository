4fde6f6286a328235551a962767033c6174d7343
haseri <iutas@bk.ru>
2018-02-21 09:43:46 +1000

    repair php-script

--- action.php
+++ action.php
@@ -7,11 +7,9 @@ $password = 'USER_PASS';
 
 // Выполнение команды в формате: 
 // curl -u 'username:password' https://api.github.com/user/repos -d '{"name":"название_проекта","description":"Описание"}'
-$create = 'curl -u ' . '\'' . $user . $password . '\'' . 'https://api.github.com/user/repos -d \' {"name":"' . $git_name . '"}\''
+$create = 'curl -u ' . '\'' . $user . ':' . $password . '\'' . ' ' . 'https://api.github.com/user/repos -d \'{"name":"' . $git_name . '"}\'';
 
 // Создаем новый json-файл
-$fp = fopen('create.json');
-// Открываем его
-fopen('create.json');
+$fp = fopen('create.json', 'w');
 // Записываем туда наш результат из $create
 fwrite($fp, $create);
