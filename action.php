c2202e8e29749e63fd19d6c8048449b5c847ebec
leskovgleb <iutas@bk.ru>
2018-02-20 23:34:30 +1000

    third commit

--- action.php
+++ action.php
@@ -7,5 +7,11 @@ $password = 'USER_PASS';
 
 // Выполнение команды в формате: 
 // curl -u 'username:password' https://api.github.com/user/repos -d '{"name":"название_проекта","description":"Описание"}'
-
 $create = 'curl -u ' . '\'' . $user . $password . '\'' . 'https://api.github.com/user/repos -d \' {"name":"' . $git_name . '"}\''
+
+// Создаем новый json-файл
+$fp = fopen('create.json');
+// Открываем его
+fopen('create.json');
+// Записываем туда наш результат из $create
+fwrite($fp, $create);
