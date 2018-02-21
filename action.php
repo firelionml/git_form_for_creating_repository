dfb20b785197ba10763700a88d3fb53f1d4d2cec
haseri <iutas@bk.ru>
2018-02-21 13:08:48 +1000

    adding ajax

--- action.php
+++ action.php
@@ -9,7 +9,9 @@ $password = 'USER_PASS';
 // curl -u 'username:password' https://api.github.com/user/repos -d '{"name":"название_проекта","description":"Описание"}'
 $create = 'curl -u ' . '\'' . $user . ':' . $password . '\'' . ' ' . 'https://api.github.com/user/repos -d \'{"name":"' . $git_name . '"}\'';
 
-// Создаем новый json-файл
-$fp = fopen('create.json', 'w');
-// Записываем туда наш результат из $create
-fwrite($fp, $create);
+if(isset($_POST['git_name']) && !empty($_POST['git_name'])) {
+	// Создаем новый json-файл
+	$fp = fopen('create.json', 'w');
+	// Записываем туда наш результат из $create
+	fwrite($fp, $create);
+}
